fb303d53e6a7548ef6df025fd85be0726fb1b093
Mathijs Nabbe <42406764+MathijsNabbe@users.noreply.github.com>
2026-02-15 21:11:19 +0100

    Make dotnet available to GitHub Actions jobs
    Set DOTNET_ROOT and update PATH for GitHub Actions.

--- entrypoint-runner.sh
+++ entrypoint-runner.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# ---- FIX: make dotnet available to GitHub Actions jobs ----
+export DOTNET_ROOT=/usr/share/dotnet
+export PATH="$DOTNET_ROOT:$PATH"
+
 cd /home/runner/actions-runner
 
 LABELS="self-hosted"
